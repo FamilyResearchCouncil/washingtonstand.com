3518881d8d81c45b1ef46b29a61382d9359385dc
tim-stuff <tim.stuff.dev@gmail.com>
2022-03-28 13:06:37 -0400

    create news listing page

---
+++ pages/news/index.js
@@ -0,0 +1,11 @@
+import HeadTag from "../../components/layout/HeadTag";
+import NewsList from "../../components/subComponents/NewsList";
+
+const NewPage = () => (
+    <>
+        <HeadTag title="News" description="" />
+        <NewsList />
+    </>
+);
+
+export default NewPage;
