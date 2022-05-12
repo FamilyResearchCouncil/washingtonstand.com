721ec0fdc17b0442a24fb51b64c1494dd9e6522a
tim-stuff <tim.stuff.dev@gmail.com>
2022-05-12 17:38:04 -0400

    make about page load WX text

--- pages/about.js
+++ pages/about.js
@@ -1,15 +1,42 @@
-import styles from "../styles/Main.module.css";
 import HeadTag from "../components/layout/HeadTag"
-import {StyledContentContainer} from "../components/layout/sections/contentContainer";
 import {StyledReadingSection} from "../components/subComponents/ReadingTextBlock";
+import DisplayPublicationHtml from "../components/subComponents/DisplayPublicationHtml";
 
 const AboutUs = (props) => (
     <>
-        <HeadTag title="About Us" description="About FRC's Association of Churches & Ministries" />
+        <HeadTag title="About Us" description="About The Washington Stand" />
 
+        <StyledReadingSection>
+            <DisplayPublicationHtml displayHtml={props.displayHtml}/>
+        </StyledReadingSection>
 
     </>
 );
+
+
+export async function getStaticProps() {
+
+    let pageProps = {}
+
+    await fetch(`https://api.frc.org/api/webtext/WX22E08.cfm?trackDownload=0`)
+        .then(res => res.text())
+        .then(
+            (result) => {
+                pageProps.displayHtml = {
+                    __html: result
+                };
+            },
+            (error) => {
+
+            }
+        );
+
+    return {
+        props: {...pageProps},
+        revalidate: 60
+    };
+
+}
 
 
 
