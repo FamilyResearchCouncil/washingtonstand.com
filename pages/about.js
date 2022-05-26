203644e0265038be86633f37a242a858efd41e31
tim-stuff <tim.stuff.dev@gmail.com>
2022-05-26 17:26:41 -0400

    add h1 tag on about page

--- pages/about.js
+++ pages/about.js
@@ -2,15 +2,30 @@ import HeadTag from "../components/layout/HeadTag"
 import {StyledReadingSection} from "../components/subComponents/ReadingTextBlock";
 import DisplayPublicationHtml from "../components/subComponents/DisplayPublicationHtml";
 import FlameImage from "../components/subComponents/FlameImage";
+import styled from "styled-components";
 
 
+const TitleH1 = styled.h1`
+  font-family: ${({theme}) => theme.fonts.titleText};
+  font-size: 4rem;
+  font-weight: 400;
+  margin: 0 0 4rem;
+  
+  span {
+    border-bottom: solid 3px ${({theme}) => theme.colors.primaryBlue};
+  }
+
+  @media (min-width: ${({ theme }) => theme.breakPoints.medium}) {
+    font-size: 5.5rem;
+  }
+`
 
 
 const AboutUs = (props) => (
     <>
         <HeadTag title="About Us" description="About The Washington Stand" />
-
         <StyledReadingSection>
+            <TitleH1><span>ABOUT</span></TitleH1>
             <DisplayPublicationHtml displayHtml={props.displayHtml}/>
             <FlameImage />
         </StyledReadingSection>
