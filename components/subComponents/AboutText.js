0914cd9201653ccd4180572e99f7b796bdccc3c9
tim-stuff <tim.stuff.dev@gmail.com>
2022-06-02 09:15:21 -0400

    make footer about text static rather than pulled from API

--- components/subComponents/AboutText.js
+++ components/subComponents/AboutText.js
@@ -16,39 +16,42 @@ const styling = {
 }
 
 const AboutBlock = (props) => {
-    const [error, setError] = useState(null);
-    const [displayHtml, setDisplayHtml] = useState({__html:"<h2>hello there</h2>"});
-    const [isLoaded, setIsLoaded] = useState(false);
-
-    useEffect(() => {
-
-        const fetchData = async () => {
-            const data = await GetAboutText();
-            setDisplayHtml(data);
-            setIsLoaded(true);
-
-        }
-
-        fetchData()
-            // make sure to catch any error
-            .catch(() => setError(true));
-
-    },[])
-
-    if (error || !isLoaded) {
-        return <div />;
-    } else {
+    // const [error, setError] = useState(null);
+    // const [displayHtml, setDisplayHtml] = useState({__html:"<h2>hello there</h2>"});
+    // const [isLoaded, setIsLoaded] = useState(false);
+    //
+    // useEffect(() => {
+    //
+    //     const fetchData = async () => {
+    //         const data = await GetAboutText();
+    //         setDisplayHtml(data);
+    //         setIsLoaded(true);
+    //
+    //     }
+    //
+    //     fetchData()
+    //         // make sure to catch any error
+    //         .catch(() => setError(true));
+    //
+    // },[])
+    //
+    // if (error || !isLoaded) {
+    //     return <div />;
+    // } else {
         return (
             <>
                 <StyledReadingSection>
                     <h2 style={styling.h2}>ABOUT</h2>
                     <TextDiv>
-                        <AboutText displayHtml={displayHtml}/>
+                        {/*<AboutText displayHtml={displayHtml}/>*/}
+                        <p>
+                            <p>The Washington Stand is Family Research Council’s outlet for news and commentary from a biblical worldview. The Washington Stand is based in Washington, D.C. and is published by FRC, whose mission is to advance faith, family, and freedom in public policy and the culture from a biblical worldview. We invite you to stand with us by&nbsp;<a href="https://www.frc.org/d-2205_73" target="_blank">partnering with FRC</a>.</p>
+                        </p>
                     </TextDiv>
                 </StyledReadingSection>
             </>
         );
-    }
+    // }
 }
 
 export default AboutBlock;
