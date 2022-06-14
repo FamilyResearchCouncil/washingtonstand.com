4009dc39404ad877437056f2d9b1cce5eaae6d83
tim-stuff <tim.stuff.dev@gmail.com>
2022-06-14 12:10:39 -0400

    update generation of sitemap to include topic pages

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,38 +1,40 @@
-const config = {
-    siteUrl: process.env.SITE_URL || 'https://washingtonstand.com',
-    changefreq: 'daily',
-    priority: 0.8,
-    sitemapSize: 5000,
-    generateRobotsTxt: true,
-    exclude: ['/newsletter/confirmation'],
-    transform: async (config, path) => {
-        return {
-            loc: path,
-            changefreq: config.changefreq,
-            priority: config.priority,
-            lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
-        }
-    },
-    additionalPaths: async (config) => [
-        await config.transform(config, '/additional-page'),
-    ],
-    robotsTxtOptions: {
-        policies: [
-            {
-                userAgent: '*',
-                allow: '/',
-            },
-            {
-                userAgent: 'test-bot',
-                allow: ['/topic', '/news','/commentary'],
-            },
-            {
-                userAgent: 'black-listed-bot',
-                disallow: ['/api','/all'],
-            },
-        ],
-        additionalSitemaps: [
-            'https://washingtonstand.com/sitemap-publications.xml'
-        ],
-    },
-}
+// const config = {
+//     siteUrl: process.env.SITE_URL || 'https://washingtonstand.com',
+//     changefreq: 'daily',
+//     priority: 0.8,
+//     sitemapSize: 5000,
+//     generateRobotsTxt: true,
+//     exclude: ['/newsletter/confirmation',],
+//     transform: async (config, path) => {
+//         return {
+//             loc: path,
+//             changefreq: config.changefreq,
+//             priority: config.priority,
+//             lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+//         }
+//     },
+//     additionalPaths: async (config) => [
+//         await config.transform(config, '/additional-page'),
+//     ],
+//     robotsTxtOptions: {
+//         policies: [
+//             {
+//                 userAgent: '*',
+//                 allow: '/',
+//             },
+//             {
+//                 userAgent: 'test-bot',
+//                 allow: ['/topic', '/news','/commentary'],
+//             },
+//             {
+//                 userAgent: 'black-listed-bot',
+//                 disallow: ['/api','/all'],
+//             },
+//         ],
+//         additionalSitemaps: [
+//             'https://washingtonstand.com/sitemap-publications.xml'
+//         ],
+//     },
+// }
+//
+// export default config
