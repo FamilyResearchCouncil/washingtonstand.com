bea48c52057806f4c3de990ecf60d46e532e19e2
tim-stuff <tim.stuff.dev@gmail.com>
2022-06-14 12:10:39 -0400

    update generation of sitemap to include topic pages

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,19 +1,54 @@
-import { getServerSideSitemapIndex, getServerSideSitemap } from 'next-sitemap';
+import { getServerSideSitemap } from 'next-sitemap';
 import GetPublications from "../helpers/GetPublications";
 
 export const getServerSideProps = async (ctx) => {
 
-    const allPublications = await GetPublications();
-    const publicationUrlList = allPublications.map(publication => {
+    let allPublications = await GetPublications();
+
+    let topicPathArray = [];
+    let topicListArray = [];
+
+    // pull a list of "topic" paths from the publication list. "lastmod" is assumed to be equal to the most recent article's publication date
+    allPublications.forEach(pub => {
+        pub.TAG_LIST.split(',').forEach(topic => {
+            if (!topicListArray.includes(topic)) {
+                let pubDate = new Date(pub.START_DATE);
+                topicListArray.push(topic);
+                topicPathArray.push({
+                    lastmod: pubDate.toISOString(),
+                    loc:`https://washingtonstand.com/topic/${topic}`
+                });
+            }
+        });
+    });
+
+    // transform top array to use the same object type as getServerSideSitemap
+    topicPathArray = topicPathArray.map(topic => {
+        return {
+            loc: topic.loc,
+            lastmod: topic.lastmod,
+            changefreq: "monthly",
+            priority: 0.6,
+            alternateRefs: [],
+            trailingSlash: false
+        }
+    });
+
+    // do the same transformation on the publications
+    allPublications = allPublications.map(publication => {
+        let pubDate = new Date(publication.START_DATE);
         return {
             loc: publication.CANONICAL_URL,
-            lastmod: publication.START_DATE,
+            lastmod: pubDate.toISOString(),
             changefreq: "never",
             priority: 0.8,
             alternateRefs: [],
             trailingSlash: false
         }
     });
+
+    // merge the 2 arrays for site map generation
+    const publicationUrlList = allPublications.concat(topicPathArray);
 
     return getServerSideSitemap( ctx, publicationUrlList );
 }
