8d77be079ed20c21a6c1127bae6ed703d8e4b6c2
rkj97 <johnson.ryan.k97@gmail.com>
2022-06-15 09:55:41 -0400

    Tried to add social icons to footer, breaks

--- components/subComponents/PublisherBlock.js
+++ components/subComponents/PublisherBlock.js
@@ -1,5 +1,12 @@
 import styled from 'styled-components';
 import PublisherLogo from "../../public/img/FRC_2017logo_white.svg";
+import {
+  InstagramIcon,
+  FacebookIcon,
+  TwitterIcon,
+} from "react-share";
+import SocialDiv from './SocialSharing';
+import SocialSharing from './SocialSharing';
 
 const Wrapper = styled.div`
   font-size: 1.2rem;
@@ -26,21 +33,26 @@ const Wrapper = styled.div`
 `
 
 const PublisherBlock = () => (
-    <>
-        <Wrapper>
-            <strong>PUBLISHED BY</strong>
-            <a href={`https://www.frc.org`}>
-            <PublisherLogo style={{ height: 100, width: 100 }} alt="Family Research Council"/>
-            </a>
-            &copy;{new Date().getFullYear()} Family Research Council<br />
-            1-800-225-4008<br />
-            801 G Street NW<br />
-            Washington, D.C. 20001
-            <div className={`linkWrapper`}>
-            <a href={`https://frc.org/privacy-policy`}>Privacy Policy</a>  |  <a href={`https://frc.org/contact-frc`}>Contact Us</a>
-            </div>
-        </Wrapper>
-    </>
+  <>
+    <Wrapper>
+      <strong>PUBLISHED BY</strong>
+      <a href={`https://www.frc.org`}>
+        <PublisherLogo style={{ height: 100, width: 100 }} alt="Family Research Council" />
+      </a>
+      &copy;{new Date().getFullYear()} Family Research Council<br />
+      1-800-225-4008<br />
+      801 G Street NW<br />
+      Washington, D.C. 20001
+      <div className={`linkWrapper`}>
+        <a href={`https://frc.org/privacy-policy`}>Privacy Policy</a>  |  <a href={`https://frc.org/contact-frc`}>Contact Us</a>
+      </div>
+      <SocialDiv>
+        <FacebookIcon borderRadius={8} size={40} href="https://www.facebook.com/WashingtonStand" />
+        <TwitterIcon borderRadius={8} size={40} href="https://twitter.com/WSHStand" />
+        <InstagramIcon borderRadius={8} size={40} href="" />
+      </SocialDiv>
+    </Wrapper>
+  </>
 );
 
 export default PublisherBlock;
