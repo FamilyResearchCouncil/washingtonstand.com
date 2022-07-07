2e8fb4dfc5962d295094731f49900157876b5fec
rkj97 <johnson.ryan.k97@gmail.com>
2022-07-07 15:43:24 -0400

    Allows menu to slide back after clicking a link or searching

--- components/layout/navigation/menu/menu.js
+++ components/layout/navigation/menu/menu.js
@@ -6,22 +6,22 @@ import appUrls from "../../../../storage/baseUrls.json";
 import ErrorBoundary from "../../../ErrorBoundary";
 import StartSearch from "../../../subComponents/StartSearch";
 
-const Menu = ({ open }) => (
+const Menu = ({ open, setOpen }) => (
         <StyledMenu open={open}>
-            <StartSearch />
+            <StartSearch open={open} setOpen={setOpen}/>
             <Link href="/">
-                <a>HOME</a>
+                <a onClick={() => setOpen(!open)}>HOME</a>
             </Link>
             <Link href="/about">
-                <a>About Us</a>
+                <a onClick={() => setOpen(!open)}>About Us</a>
             </Link>
             <Link href="/authors">
-                <a>Authors</a>
+                <a onClick={() => setOpen(!open)}>Authors</a>
             </Link>
-            <a href="mailto:tips@washingtonstand.com?subject=Story tip for The Washington Stand" target={`_blank`}>
+            <a href="mailto:tips@washingtonstand.com?subject=Story tip for The Washington Stand" target={`_blank`} onClick={() => setOpen(!open)}>
                 Tips
             </a>
-            <a href={appUrls.external.donate} target={`_blank`}>
+            <a href={appUrls.external.donate} target={`_blank`} onClick={() => setOpen(!open)}>
                 Donate
             </a>
             <div className={`topicNews`}>
