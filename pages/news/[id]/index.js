239fea8b00fa676b5c7847744913fd36cb9eda6f
tim-stuff <tim.stuff.dev@gmail.com>
2022-07-08 12:54:20 -0400

    limit the number of indidvidual articles generated at build time

--- pages/news/[id]/index.js
+++ pages/news/[id]/index.js
@@ -124,7 +124,7 @@ export const getStaticPaths = async () => {
 
     const publications = await GetPublications(['NA']);
 
-    const trimmedPublicationList = (publications.length > 80) ? publications.slice(0,80) : publications;
+    const trimmedPublicationList = (publications.length > 40) ? publications.slice(0,40) : publications;
 
     const publishPathsArray = trimmedPublicationList.map((pub) => ({
         params: { id: pub.URL_SLUG}
